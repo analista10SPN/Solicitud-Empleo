34f9ecbed80895fb81b93038f9336d41bb34cf4a
analista10@spn.com.do <analista10@spn.com.do>
2022-11-07 19:36:48 -0400

    Alert + Required Fields Modal addeed, need to pollish responsiveness

---
+++ src/pages/components/requiredFieldModal.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+type requiredFieldProps = {
+    open:boolean,
+    title: string,
+    field: string,
+    message: string,
+    setClose: ()=>void
+
+}
+
+export default function RequiredFieldModal({open,title,message,field,setClose}:requiredFieldProps) {
+  if(!open) return null
+    return (
+<>
+<div id="defaultModal" tabIndex={-1} aria-hidden="true" className="overflow-y-auto overflow-x-hidden fixed top-0 lg:top-[10%] right-0 left-0 lg:left-[33%] z-50 w-full md:inset-0 h-modal md:h-full">
+    <div className="relative lg:left-[25%] lg:top-[10%] p-4 w-full max-w-2xl h-full md:h-auto">
+        {/* <!-- Modal content --> */}
+        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            {/* <!-- Modal header --> */}
+            <div className="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
+                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                    {title}
+                </h3>
+                <button onClick={setClose} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+                    <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    <span className="sr-only">Close modal</span>
+                </button>
+            </div>
+            {/* <!-- Modal body --> */}
+            <div className="p-6 space-y-6">
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                        {message}: <p className="text-base leading-relaxed text-red-500 dark:text-gray-400">{field}</p>
+                </p>
+ 
+            </div>
+            {/* Modal Footer */}
+            <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+                <button onClick={setClose} data-modal-toggle="defaultModal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Entendido</button>
+            </div>
+        </div>
+    </div>
+
+</div>
+ <div className="opacity-25 fixed inset-0 z-40 bg-black" onClick={setClose}></div>
+</>
+  )
+}
